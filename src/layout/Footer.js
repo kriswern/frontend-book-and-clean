7d07b13b31ff80024fe61f377858c01ec103a899
fredrik1992 <klosansa1@.com>
2021-12-02 19:15:27 +0100

    small change to css

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,6 +1,7 @@
 export default function Footer (){
     return(
         <footer class=" text-center text-lg-start">
-            <h2>to be added</h2>
+            <h2>made as a part of a course at Ec-utbildning</h2>
+            <p>By:Fredrik,Oskar,Saurabh,Kristoffer</p>
         </footer>  )
 }
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,10 +5,23 @@ export default function Navbar (){
         
 
         <nav className = "nav">
+            <h2 className  ="bookIcon">Book</h2>
             <ul className = "navList">
-                 <li className = "link-primary navlistItem"><a>home</a></li>
-                 <li className = "link-primary navlistItem"><a>admin</a></li>
-                <li className = "link-primary navlistItem"><a>placeholder</a></li>
+                 <li className = "link-primary navlistItem">
+                     <button>
+                         <a class="nav-link">home</a>
+                    </button>
+                </li>
+                <li className = "link-primary navlistItem">
+                    <button>
+                        <a class="nav-link test">admin</a>
+                    </button>
+                </li>
+                <li className = "link-primary navlistItem">
+                    <button>
+                        <a class="nav-link">placeholder</a>
+                    </button>
+                </li>
              </ul>
         </nav>
         
