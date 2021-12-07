ff2976ffa9ecbdaa50cd12b9f2cfe402d7af5c50
Coin-byte <kriswern@icloud.com>
2021-12-07 16:24:13 +0100

    Simple login, WIP

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "../../css/login.css";
 
 export default function Login() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameInput = (e) => {
+    setUsername(e.target.value)
+}
+const passwordInput = (e) => {
+  setPassword(e.target.value)
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  axios.post('http://localhost:8080/login', {
+    username: username,
+    password: password
+  })
+  .then(function (response) {
+    console.log(response);
+    setUsername("")
+    setPassword("")
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
   return (
     <div class="login-form-container">
       <div class="card w-25 align-self-center">
@@ -9,11 +37,13 @@ export default function Login() {
           <div class="form-group">
             <label for="inputEmail">Email address</label>
             <input
-              type="email"
+              type="text"
               class="form-control"
               id="inputEmail"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              value={username}
+              onChange={usernameInput}
             />
             <small id="emailHelp" class="form-text text-muted">
               We'll never share your email with anyone else.
@@ -26,10 +56,12 @@ export default function Login() {
               class="form-control"
               id="inputPassword"
               placeholder="Password"
+              value={password}
+              onChange={passwordInput}
               required
             />
           </div>
-          <button type="submit" class="btn btn-primary mt-2">
+          <button type="submit" class="btn btn-primary mt-2" onClick={handleSubmit}>
             Submit
           </button>
         </form>
