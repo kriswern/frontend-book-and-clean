55e6934ce0782d89155ea8c3ed3595ed39a0a6ac
fredrik1992 <klosansa1@.com>
2021-12-08 11:41:35 +0100

    added admin page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import Layout from './components/Layout';
 import CustomerPage from './components/Pages/customerPage/CustomerPage';
 import CleanerPage from './components/Pages/cleanerPage/CleanerPage';
-
+import AdminPage from './components/Pages/adminPage/AdminPage'
 
 function App() {
   return (
    <Layout>
-     <CustomerPage />
+    <AdminPage/>
 
    </Layout>
   );
---
+++ src/components/Pages/adminPage/AllBookingsAdmin.js
@@ -0,0 +1,13 @@
+import Bookings from "../../booking/Bookings";
+
+
+
+export default function AdminPage() {
+    return(
+            
+        <div>
+            <br></br>
+        <Bookings/>
+        </div>
+    )
+}
