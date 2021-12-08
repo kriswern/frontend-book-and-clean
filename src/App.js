b14ea4e67c436c0bb76b08ed196ab84337d508f2
saurabh311 <saurabh311@gmail.com>
2021-12-09 00:48:24 +0100

    fix role wise access

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Layout from './components/Layout';
 import CustomerPage from './components/Pages/customerPage/CustomerPage';
 import CleanerPage from './components/Pages/cleanerPage/CleanerPage';
 import AdminPage from './components/Pages/adminPage/AdminPage'
+import RegisterForm from './components/register/RegisterForm'
 
 function App() {
   return (
    <Layout>
     <AdminPage/>
+    <RegisterForm/>
 
    </Layout>
   );
