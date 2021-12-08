eac0811d66d746fd9af0eb88f7989d2291b25db7
saurabh311 <saurabh311@gmail.com>
2021-12-08 19:15:09 +0100

    enable response alert for registration success in backend

--- src/services/RegisterService.js
+++ src/services/RegisterService.js
@@ -8,7 +8,11 @@ class RegisterService{
     registerUser(registerForm) {
     console.log(registerForm)
     axios.post(USER_REGISTER_REST_API_URL, registerForm).then((response) => {
-      if(response.status === 200) {
+      if(response.status === 201) {
+        alert("Welcome "+ registerForm.name +" you are registered !" );
+        return true;
+      }else if(response.status === 208){
+        alert("Error..... "+registerForm.email+ " email Id is already registered. " );
         return true;
       }
     });
