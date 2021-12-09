f4dec2f243ea893dc5444e1eef880ab33b61f670
saurabh311 <saurabh311@gmail.com>
2021-12-09 22:39:42 +0100

    get response the from Backend as http response

--- src/services/RegisterService.js
+++ src/services/RegisterService.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 
-const USER_REGISTER_REST_API_URL = 'http://localhost:8080/register'
+const USER_REGISTER_REST_API_URL = "http://localhost:8080/register";
 
-
-class RegisterService{
-
+class RegisterService {
     registerUser(registerForm) {
-    console.log(registerForm)
-    axios.post(USER_REGISTER_REST_API_URL, registerForm).then((response) => {
-      if(response.status === 201) {
-        alert("Welcome "+ registerForm.name +" you are registered !" );
-        return true;
-      }else if(response.status === 208){
-        alert("Error..... "+registerForm.email+ " email Id is already registered. " );
-        return true;
-      }
-    });
-  }
+        console.log(registerForm);
+        axios
+            .post(USER_REGISTER_REST_API_URL, registerForm)
+            .then((response) => {
+                if (response.status === 201) {
+                    alert(
+                        "Welcome " + registerForm.name + " you are registered !"
+                    );
+                    return true;
+                } else if (response.status === 208) {
+                    alert(
+                        "Error..... " +
+                            registerForm.email +
+                            " email Id is already registered. "
+                    );
+                    return false;
+                }
+            });
+    }
 }
 
 export default new RegisterService();
