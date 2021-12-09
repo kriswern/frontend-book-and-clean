f38c73cfd69208cb387bc8c7f4d846ab978c00d7
saurabh311 <saurabh311@gmail.com>
2021-12-09 22:40:49 +0100

    fix CSS part and role access

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import RegisterService from "../../services/RegisterService";
+import "../../css/registerForm.css";
 
 export default function RegisterForm() {
     const initialState = {
@@ -11,7 +12,6 @@ export default function RegisterForm() {
     };
 
     const [formData, setFormData] = useState(initialState);
-    const [role, setRole] = useState("admin");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,107 +21,137 @@ export default function RegisterForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Name</label>
+        <div className="form-container">
+            <form
+                onSubmit={handleSubmit}
+                className="register-form d-flex flex-column align-content-center">
+                <div class="form-group-row">
+                    <label
+                        for="validationTooltip01"
+                        class="col-sm-2 col-form-label"
+                    >
+                        Name
+                    </label>
+                    <div class="col-sm-10">
+                        <input
+                            value={formData.name}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    name: e.target.value,
+                                })
+                            }
+                            type="text"
+                            class="form-control"
+                            id="validationTooltip01"
+                            placeholder=""
+                            required
+                        />
+                    </div>
+                </div>
+                <div class="form-group-row">
+                    <label for="inputEmail4" class="col-sm-2 col-form-label">
+                        Email
+                    </label>
+                    <div class="col-sm-10">
+                        <input
+                            value={formData.email}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    email: e.target.value,
+                                })
+                            }
+                            type="email"
+                            class="form-control"
+                            id="inputEmail4"
+                            placeholder=""
+                            required
+                        />
+                    </div>
+                </div>
+                <div class="form-group-row">
+                    <label
+                        for="inputPassword4"
+                        class="col-sm-2 col-form-label ">
+                        Password
+                    </label>
+                    <div class="col-sm-10">
+                        <input
+                            value={formData.password}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    password: e.target.value,
+                                })
+                            }
+                            type="password"
+                            class="form-control"
+                            id="inputPassword4"
+                            placeholder=""
+                            required
+                        />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputAddress" class="col-sm-2 col-form-label">
+                        Address
+                    </label>
+                    <div class="col-sm-10">
+                        <input
+                            value={formData.address}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    address: e.target.value,
+                                })
+                            }
+                            type="text"
+                            class="form-control"
+                            id="inputAddress"
+                            placeholder=""
+                            required
+                        />
+                    </div>
+                </div>
+                <br />
+                <div class="form-check">
                     <input
-                        value={formData.name}
+                        class="form-check-input"
+                        type="radio"
+                        name="exampleRadios"
+                        id="exampleRadios1"
+                        value="customer"
                         onChange={(e) =>
-                            setFormData({ ...formData, name: e.target.value })
+                            setFormData({ ...formData, type: e.target.value })
                         }
-                        type="text"
-                        class="form-control"
-                        id="inputName4"
-                        placeholder="Name"
                         required
                     />
+                    <label class="form-check-label" for="exampleRadios1">
+                        Cutomer
+                    </label>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
+                <div class="form-check">
                     <input
-                        value={formData.email}
+                        class="form-check-input"
+                        type="radio"
+                        name="exampleRadios"
+                        id="exampleRadios2"
+                        value="cleaner"
                         onChange={(e) =>
-                            setFormData({ ...formData, email: e.target.value })
+                            setFormData({ ...formData, type: e.target.value })
                         }
-                        type="email"
-                        class="form-control"
-                        id="inputEmail4"
-                        placeholder="Email"
                         required
                     />
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input
-                        value={formData.password}
-                        onChange={(e) =>
-                            setFormData({
-                                ...formData,
-                                password: e.target.value,
-                            })
-                        }
-                        type="password"
-                        class="form-control"
-                        id="inputPassword4"
-                        placeholder="Password"
-                        required
-                    />
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input
-                    value={formData.address}
-                    onChange={(e) =>
-                        setFormData({ ...formData, address: e.target.value })
-                    }
-                    type="text"
-                    class="form-control"
-                    id="inputAddress"
-                    placeholder=""
-                    required
-                />
-            </div>
-            <br />
-            <div> 
-            <div class="form-check"> 
-                <input
-                    class="form-check-input"
-                    type="radio"
-                    name="exampleRadios"
-                    id="exampleRadios1"
-                    value="Customer"
-                    onChange={(e) =>
-                        setFormData({ ...formData, type: e.target.value })
-                    }
-                    required
-                />
-                <label class="form-check-label" for="exampleRadios1">
-                    Cutomer
-                </label>
-            </div> {role === "admin" &&
-            <div class="form-check">
-                <input
-                    class="form-check-input"
-                    type="radio"
-                    name="exampleRadios"
-                    id="exampleRadios2"
-                    value="Cleaner"
-                    onChange={(e) =>
-                        setFormData({ ...formData, type: e.target.value })
-                    }
-                    required
-                />
-                <label class="form-check-label" for="exampleRadios2">
-                    Cleaner
-                </label>
-            </div>}
-            </div>
-            <br />
-            <button type="submit" class="btn btn-primary">
-                Register
-            </button>
-        </form>
+                    <label class="form-check-label" for="exampleRadios2">
+                        Cleaner
+                    </label>
+                </div>{" "}
+                <br />
+                <button type="submit" class="btn btn-primary">
+                    Register
+                </button>
+            </form>
+        </div>
     );
 }
