d49805dadfd81de4b6244d0e0a27fb1f09657d6e
Coin-byte <kriswern@icloud.com>
2021-12-12 18:51:50 +0100

    Logs out and redirects to login

--- src/components/Pages/userNav.js
+++ src/components/Pages/userNav.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "../../css/userNav.css"
 
 export default function UserNav({ routes }) {
+  const history = useHistory();
   
   const [navOpen, setNavOpen] = useState(false);
  
   function openOrCloseNav() {
     navOpen === false ? setNavOpen(true) : setNavOpen(false);
+  }
+
+  const logOut = () => {
+    localStorage.removeItem("access_token")
+    openOrCloseNav()
+    history.push('/login')
   }
 
   if (navOpen) {
@@ -25,6 +32,8 @@ export default function UserNav({ routes }) {
             <Link key={index} to={route.path} className="list-item"><button className="menu-button list-item">{route.name}</button></Link>
             
           ))}
+          <br/>
+          <li className="list-item"><button className="menu-button list-item" onClick={logOut}>Logout</button></li>
         </ul>
       </aside>
     );
