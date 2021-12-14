185464061a50bef69c0ff20ac21d3023aaa087b0
Oskar Andersson <oskand01@gmail.com>
2021-12-14 21:36:00 +0100

    fix logic

--- src/services/DateService.js
+++ src/services/DateService.js
@@ -3,8 +3,8 @@ class DateService {
   //Atleast twelve hours newer than the current date
   isDateNewer = (date) => {
     const twelveHours = 1000 * 60 * 60 * 12;
-    const now = Date.now();
-    const bookingDate = Date.parse(date) + twelveHours;
+    const now = Date.now() + twelveHours;
+    const bookingDate = Date.parse(date);
     return bookingDate > now ? true : false;
   };
 }
